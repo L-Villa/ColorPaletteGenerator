b53bb1b424ec596ac4f3e4a3b54303aa03309b5e
L-Villa <63620359+L-Villa@users.noreply.github.com>
2020-04-15 01:59:54 -0700

    added color theory math for consistent palettes

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ const generateBtn = document.querySelector(".generate");
 const sliders = document.querySelectorAll('input[type="range"]');
 const currentHexes = document.querySelectorAll(".color h2");
 const popup = document.querySelector(".copy-container");
+const controls = document.querySelectorAll(".controls");
 const adjustButton = document.querySelectorAll(".adjust");
 const lockButton = document.querySelectorAll(".lock");
 const closeAdjustments = document.querySelectorAll(".close-adjustment");
@@ -14,73 +15,295 @@ let initialColors;
 let savedPalettes = [];
 
 //event listeners
-generateBtn.addEventListener("click", randomColors);
-
+generateBtn.addEventListener("click", generateColorScheme);
+window.addEventListener("keydown", function (e) {
+  if (e.keyCode === 32) {
+    generateColorScheme();
+    e.preventDefault();
+  }
+});
 sliders.forEach((slider) => {
   slider.addEventListener("input", hslControls);
 });
-
-colorDivs.forEach((div, index) => {
+colorDivs.forEach((div, index, e) => {
   div.addEventListener("change", () => {
     updateTextUI(index);
   });
+  div.addEventListener("click", () => {
+    toggleLock(index);
+  });
 });
-
 currentHexes.forEach((hex) => {
   hex.addEventListener("click", () => {
     copyToClipboard(hex);
   });
 });
-
 popup.addEventListener("transitionend", () => {
   const popupBox = popup.children[0];
   popup.classList.remove("active");
   popupBox.classList.remove("active");
 });
-
 adjustButton.forEach((button, index) => {
-  button.addEventListener("click", () => {
+  button.addEventListener("click", (e) => {
     openAdjustmentPanel(index);
+    e.stopPropagation();
   });
 });
-
-lockButton.forEach((button, index) => {
-  button.addEventListener("click", () => {
-    toggleLock(index);
-  });
-});
-
 closeAdjustments.forEach((button, index) => {
-  button.addEventListener("click", () => {
+  button.addEventListener("click", (e) => {
     closeAdjustmentPanel(index);
+    e.stopPropagation();
   });
 });
+// lockButton.forEach((button, index) => {
+//   button.addEventListener('click', (e) => {
+//     toggleLock(index);
+//     e.stopPropagation();
+//   })
+// })
 
-//Functions
-function generateHex() {
-  const hexColor = chroma.random().hex();
+// Functions ------------------------------------------------
+
+function hexToHSL(H) {
+  const HSLColor = chroma(H).hsl();
+  const h = HSLColor[0];
+  const s = HSLColor[1];
+  const l = HSLColor[2];
+  const hslArray = [h, s, l];
+  return hslArray;
+}
+
+function HSLToHex(hslArray) {
+  const hexColor = chroma.hsl(hslArray[0], hslArray[1], hslArray[2]).hex();
   return hexColor;
 }
 
-function randomColors() {
+let complimentaryColor = false;
+let monochromaticColor = false;
+let analogousColor = false;
+let splitComplimentaryColor = false;
+let triadicColor = false;
+let tetradicColor = false;
+
+function randomColorSchemeType() {
+  randNo = Math.floor(Math.random() * 6) + 1;
+  complimentaryColor = false;
+  monochromaticColor = false;
+  analogousColor = false;
+  splitComplimentaryColor = false;
+  triadicColor = false;
+  tetradicColor = false;
+  switch (randNo) {
+    case 1:
+      complimentaryColor = true;
+      break;
+    case 2:
+      monochromaticColor = true;
+      break;
+    case 3:
+      analogousColor = true;
+      break;
+    case 4:
+      splitComplimentaryColor = true;
+      break;
+    case 5:
+      triadicColor = true;
+      break;
+    case 6:
+      tetradicColor = true;
+      break;
+    default:
+      monochromaticColor = true;
+  }
+}
+
+function randDecimal() {
+  const randNo = Math.random() * 0.5;
+  return randNo;
+}
+
+function generateHArrays() {
+  hs = [];
+  ss = [];
+  ls = [];
+  for (let i = 0; i < 3; i++) {
+    hs[i] = mainColorArray[0];
+    ss[i] = mainColorArray[1];
+    ls[i] = mainColorArray[2];
+  }
+  return [hs, ss, ls];
+}
+
+function generateColorScheme() {
+  // const numberOfColors = Math.floor(Math.random() * 2) + 2;
   initialColors = [];
+  mainColors = [];
+  accentColors = [];
+  colorLuminance = [];
+  const randSchemeType = randomColorSchemeType();
+  const randomHex = generateRandomColor();
+  mainColorArray = hexToHSL(randomHex);
+
+  // generateHArrays();
+  // console.log(hs);
+  // console.log(ss);
+  // console.log(ls);
+  let h1;
+  let s1;
+  let l1;
+  let h2;
+  let s2;
+  let l2;
+  let h3;
+  let s3;
+  let l3;
+  let hslString1;
+  let hslString2;
+  let hslString3;
+  h1Array = [];
+  h2Array = [];
+  h3Array = [];
+  h1Color = [];
+  mainColor = [HSLToHex(mainColorArray)];
+  secondColor = [];
+  thirdColor = [];
+  fourthColor = [];
+  if (complimentaryColor) {
+    //----------This 150 should be 180 but that is not giving me right color against blue
+    h1 = Math.abs(mainColorArray[0] + 180 - 360);
+    s1 = Math.abs(mainColorArray[1] - randDecimal());
+    l1 = Math.abs(mainColorArray[2] - randDecimal());
+    hslString1 = "hsl(" + h1 + "," + s1 * 100 + "%," + l1 * 100 + "%)";
+    h1Array.push(h1, s1, l1);
+    secondColor = [HSLToHex(h1Array)];
+  } else if (monochromaticColor) {
+    h1 = Math.abs(mainColorArray[0] - randDecimal() * 5);
+    s1 = Math.abs(mainColorArray[1] - randDecimal());
+    l1 = Math.abs(mainColorArray[2] - randDecimal());
+    hslString1 = "hsl(" + h1 + "," + s1 * 100 + "%," + l1 * 100 + "%)";
+    h1Array.push(h1, s1, l1);
+    secondColor = [HSLToHex(h1Array)];
+  } else if (analogousColor) {
+    h1 = Math.abs(mainColorArray[0] + 60 - 360);
+    s1 = Math.abs(mainColorArray[1] - randDecimal());
+    l1 = Math.abs(mainColorArray[2] - randDecimal());
+    h2 = Math.abs(mainColorArray[0] + 120 - 360);
+    s2 = Math.abs(mainColorArray[1] - randDecimal());
+    l2 = Math.abs(mainColorArray[2] - randDecimal());
+    hslString1 = "hsl(" + h1 + "," + s1 * 100 + "%," + l1 * 100 + "%)";
+    hslString2 = "hsl(" + h2 + "," + s1 * 100 + "%," + l2 * 100 + "%)";
+    h1Array = [h1, s1, l1];
+    h2Array = [h2, s2, l2];
+    secondColor = [HSLToHex(h1Array)];
+    thirdColor = [HSLToHex(h2Array)];
+  } else if (splitComplimentaryColor) {
+    h1 = Math.abs(mainColorArray[0] + 150 - 360);
+    s1 = Math.abs(mainColorArray[1] - randDecimal());
+    l1 = Math.abs(mainColorArray[2] - randDecimal());
+    h2 = Math.abs(mainColorArray[0] + 210 - 360);
+    s2 = Math.abs(mainColorArray[1] - randDecimal());
+    l2 = Math.abs(mainColorArray[2] - randDecimal());
+    hslString1 = "hsl(" + h1 + "," + s1 * 100 + "%," + l1 * 100 + "%)";
+    hslString2 = "hsl(" + h2 + "," + s1 * 100 + "%," + l2 * 100 + "%)";
+    h1Array = [h1, s1, l1];
+    h2Array = [h2, s2, l2];
+    secondColor = [HSLToHex(h1Array)];
+    thirdColor = [HSLToHex(h2Array)];
+  } else if (triadicColor) {
+    h1 = Math.abs(mainColorArray[0] + 120 - 360);
+    s1 = Math.abs(mainColorArray[1] - randDecimal());
+    l1 = Math.abs(mainColorArray[2] - randDecimal());
+    h2 = Math.abs(mainColorArray[0] + 240 - 360);
+    s2 = Math.abs(mainColorArray[1] - randDecimal());
+    l2 = Math.abs(mainColorArray[2] - randDecimal());
+    hslString1 = "hsl(" + h1 + "," + s1 * 100 + "%," + l1 * 100 + "%)";
+    hslString2 = "hsl(" + h2 + "," + s1 * 100 + "%," + l2 * 100 + "%)";
+    h1Array = [h1, s1, l1];
+    h2Array = [h2, s2, l2];
+    secondColor = [HSLToHex(h1Array)];
+    thirdColor = [HSLToHex(h2Array)];
+  } else if (tetradicColor) {
+    h1 = Math.abs(mainColorArray[0] + 90 - 360);
+    s1 = Math.abs(mainColorArray[1] - randDecimal());
+    l1 = Math.abs(mainColorArray[2] - randDecimal());
+    h2 = Math.abs(mainColorArray[0] + 180 - 360);
+    s2 = Math.abs(mainColorArray[1] - randDecimal());
+    l2 = Math.abs(mainColorArray[2] - randDecimal());
+    h3 = Math.abs(mainColorArray[0] + 270 - 360);
+    s3 = Math.abs(mainColorArray[1] - randDecimal());
+    l3 = Math.abs(mainColorArray[2] - randDecimal());
+    hslString1 = "hsl(" + h1 + "," + s1 * 100 + "%," + l1 * 100 + "%)";
+    hslString2 = "hsl(" + h2 + "," + s1 * 100 + "%," + l3 * 100 + "%)";
+    hslString3 = "hsl(" + h3 + "," + s1 * 100 + "%," + l2 * 100 + "%)";
+    h1Array = [h1, s1, l1];
+    h2Array = [h2, s2, l2];
+    h3Array = [h3, s3, l3];
+    secondColor = [HSLToHex(h1Array)];
+    thirdColor = [HSLToHex(h2Array)];
+    fourthColor = [HSLToHex(h3Array)];
+  } else {
+    console.log("Did not understand");
+  }
+  if (fourthColor.length > 0) {
+    primaryColors = [
+      ...mainColor,
+      ...secondColor,
+      ...thirdColor,
+      ...fourthColor,
+    ];
+  } else if (thirdColor.length > 0) {
+    primaryColors = [...mainColor, ...secondColor, ...thirdColor];
+  } else {
+    primaryColors = [...mainColor, ...secondColor];
+  }
+  const numberOfColors = primaryColors.length;
+  const numberOfAccents = 5 - numberOfColors;
+  for (i = 0; i < numberOfAccents; i++) {
+    chooseColor = Math.floor(Math.random() * numberOfColors);
+    saturationRandNo = Math.random() * 3;
+    darkenRandNo = Math.random() * 4;
+    accentColor = primaryColors[chooseColor];
+    const lum = chroma(accentColor).luminance();
+    if (lum >= 0.8 && lum < 1) {
+      accentColorSaturation = chroma(accentColor)
+        .desaturate(saturationRandNo)
+        .darken(darkenRandNo)
+        .hex();
+    } else if (lum >= 0.5 && lum < 0.8) {
+      accentColorSaturation = chroma(accentColor)
+        .desaturate(saturationRandNo)
+        .darken(darkenRandNo)
+        .hex();
+    } else if (lum >= 0.3 && lum < 0.5) {
+      accentColorSaturation = chroma(accentColor)
+        .saturate(saturationRandNo)
+        .brighten(darkenRandNo)
+        .hex();
+    } else {
+      accentColorSaturation = chroma(accentColor)
+        .saturate(saturationRandNo)
+        .brighten(darkenRandNo)
+        .hex();
+    }
+    accentColors.push(accentColorSaturation);
+  }
+  finalColors = [...primaryColors, ...accentColors];
   colorDivs.forEach((div, index) => {
     const hexText = div.children[0];
-    const randomColor = generateHex();
     //add color to array
     if (div.classList.contains("locked")) {
       initialColors.push(hexText.innerText);
       return;
     } else {
-      initialColors.push(chroma(randomColor).hex());
+      initialColors.push(finalColors[index]);
     }
     //add color to bg
-    div.style.backgroundColor = randomColor;
-    hexText.innerText = randomColor;
+    div.style.backgroundColor = finalColors[index];
+    hexText.innerText = finalColors[index];
     //check for contrast
-    checkTextContrast(randomColor, hexText);
+    checkTextContrast(finalColors[index], hexText);
     //initial colorize sliders
-    const color = chroma(randomColor);
+    const color = chroma(finalColors[index]);
     const sliders = div.querySelectorAll(".sliders input");
     const hue = sliders[0];
     const brightness = sliders[1];
@@ -96,12 +319,17 @@ function randomColors() {
   });
 }
 
+function generateRandomColor() {
+  const H = chroma.random().hex();
+  return H;
+}
+
 function checkTextContrast(color, text) {
   const luminance = chroma(color).luminance();
   if (luminance > 0.5) {
-    text.style.color = "rgb(75,75,75)";
+    text.style.color = "rgb(30,30,30)";
   } else {
-    text.style.color = "rgb(200,200,200)";
+    text.style.color = "rgb(220,220,220)";
   }
 }
 
@@ -190,17 +418,21 @@ function copyToClipboard(hex) {
 }
 
 function openAdjustmentPanel(index) {
+  controls[index].children[0].classList.toggle('active');
   sliderContainers[index].classList.toggle("active");
 }
 
 function closeAdjustmentPanel(index) {
+  controls[index].children[0].classList.toggle('active');
   sliderContainers[index].classList.remove("active");
 }
 
 function toggleLock(index) {
   const currentBg = colorDivs[index];
   const lockSVG = lockButton[index].children[0];
+  const locker = lockButton[index];
   currentBg.classList.toggle("locked");
+  locker.classList.toggle('active');
   if (lockSVG.classList.contains("fa-lock-open")) {
     lockSVG.classList.add("fa-lock");
   } else {
@@ -365,6 +597,9 @@ function getLocal() {
   }
 }
 
-//calling functions
-getLocal();
-randomColors();
+function init() {
+  getLocal();
+  generateColorScheme();
+}
+
+init();
